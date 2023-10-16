f1be6f1521ec52980f26ece2bd7823fab652f47f
Octo24 <147954002+Octo24@users.noreply.github.com>
2023-10-16 13:53:38 -0600

    step 1
    step 1

---
+++ temp.py
@@ -0,0 +1,8 @@
+# convert 25C degrees to F degrees
+# Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
+
+if CtoF:
+  print(f"Celcius to Fahrenheit: {C}C = {CtoF()}F")
+elif FtoC:
+  print(f"Fahrenheit to Celcius: {F}F = {FtoC()}C")
+else:
